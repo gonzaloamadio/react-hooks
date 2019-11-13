6c79b7506ebce765b07ba2ff62aa6c6589f34482
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-11-13 00:31:07 -0300

    Remove Ingredient. Handler

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = ingr => {
+    // setIngredients(prevIngredients => [...prevIngredients, ingr])
+    // For now, create dummy id, then will be brought from server.
+    setIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingr }
+    ]);
+  };
+
+  const removeIngredientHandler = id => {
+    setIngredients(prevIngredients =>
+      // prevIngredients.filter(function(value, index, arr) {
+      prevIngredients.filter(function(ingredient) {
+        return ingredient.id !== id;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
