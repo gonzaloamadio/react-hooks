c6d1d834a1fd9e1a4e53d5b221fa9cd5febf4a21
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-11-13 13:43:07 -0300

    useRef to an element. Search when user stop typing

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -9,37 +9,50 @@ const Search = React.memo(props => {
   // If we use props in the array, it will be all props.
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   // To search, we can register a function that is executed on each function stroke.
   // On the onChange, a handler that exec setEnteredFilter and also fetch from DB and search.
   // Or ... useEffect , and split the logic in a more elegant way.
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch("https://react-hooks-23a01.firebaseio.com/ingredients.json" + query)
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+    // We want to look for values if the user has stopped writing, not for every key stroke.
+    // So with timeOut, if you stop writing for 1 second, we assume you stop searching
+    setTimeout(() => {
+      // enteredFilter will be locked in when we setTimeout, so it will not be the current one
+      // it will be the "old" one.
+      // To get the current value, we use a reference.
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-hooks-23a01.firebaseio.com/ingredients.json" + query
+        )
+          .then(response => response.json())
+          .then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            // After we search, we should pass this info to Ingredients.js, where we show the ingredients.
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        // After we search, we should pass this info to Ingredients.js, where we show the ingredients.
-        onLoadIngredients(loadedIngredients);
-      });
-    // This will be exec, every time enteredFilter changes. i.e on every key stroke.
+      }
+    }, 1000);
+
+    // This will be exec, every time enteredFilter changes.
     // Or when onLoadIngredients changes. It is a function, it can change.. but it should not.
     // BUT.. if we use it like this, every time the component that is providing onLoadIngredients
     // is rendered, will create a new onLoadIngredients function, so this will be re executed,
     // and will cause an inifinite loop.
     // The solution is wrap onLoadIngredients with useCallback hook (in the component where it is
     // defined, i.e. Ingredients.js)
-  }, [enteredFilter, onLoadIngredients]);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -47,6 +60,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={event => setEnteredFilter(event.target.value)}
