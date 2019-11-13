3ef8ca1e316b0c9aa42a85aa7b63f2abe977b9f1
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-11-13 13:04:11 -0300

    useEffect and useCallback to search ingredients.

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,20 +7,23 @@ import Search from "./Search";
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch("https://react-hooks-23a01.firebaseio.com/ingredients.json")
-      .then(response => response.json())
-      .then(responseData => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setIngredients(loadedIngredients);
-      });
+  // We do not need this on first renderd. Search will do it.
+  // If we leave it, we will have to http requests on component startup.
+  //
+  // useEffect(() => {
+  //   fetch("https://react-hooks-23a01.firebaseio.com/ingredients.json")
+  //      ...
+  //       setIngredients(loadedIngredients);
+  //     });
+  // }, []);
+
+  // If we do not use useCallback, every time this component renders, will cause
+  // a new reference of filterIngredientsHandler passed on to Search component.
+  // This function is on a dependency array of useEffect on Search component,
+  // and will cause a search of ingredients, and re render this one. So an infinite loop
+  // With useCallback, this function is cached, preventing this.
+  const filterIngredientsHandler = useCallback(filterIngredients => {
+    setIngredients(filterIngredients);
   }, []);
 
   const addIngredientHandler = ingr => {
@@ -54,7 +57,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filterIngredientsHandler} />
         <IngredientList
           ingredients={ingredients}
           onRemoveItem={removeIngredientHandler}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
 const Search = React.memo(props => {
+  // As we use props inside useEffect, we need to do this so we can use onLoadIngredients
+  // in the dependency array of useEffect. Because we want to use this specific props.
+  // If we use props in the array, it will be all props.
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  // To search, we can register a function that is executed on each function stroke.
+  // On the onChange, a handler that exec setEnteredFilter and also fetch from DB and search.
+  // Or ... useEffect , and split the logic in a more elegant way.
+  useEffect(() => {
+    const query =
+      enteredFilter.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch("https://react-hooks-23a01.firebaseio.com/ingredients.json" + query)
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          });
+        }
+        // After we search, we should pass this info to Ingredients.js, where we show the ingredients.
+        onLoadIngredients(loadedIngredients);
+      });
+    // This will be exec, every time enteredFilter changes. i.e on every key stroke.
+    // Or when onLoadIngredients changes. It is a function, it can change.. but it should not.
+    // BUT.. if we use it like this, every time the component that is providing onLoadIngredients
+    // is rendered, will create a new onLoadIngredients function, so this will be re executed,
+    // and will cause an inifinite loop.
+    // The solution is wrap onLoadIngredients with useCallback hook (in the component where it is
+    // defined, i.e. Ingredients.js)
+  }, [enteredFilter, onLoadIngredients]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
